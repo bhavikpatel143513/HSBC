7ee9bb8cc29851d0ba1c67643c334d932dbd8e09
Bhavik Patel <bhavikpatel143513@gmail.com>
2020-10-30 11:58:41 +0530

    Get Post Put Delete Mappings
    Using Advanced REST Client for testing post, put, delete, etc.

--- springbootfirst/src/main/java/com/springbootfirst/controller/FbController.java
+++ springbootfirst/src/main/java/com/springbootfirst/controller/FbController.java
@@ -2,7 +2,10 @@ package com.springbootfirst.controller;
 
 import java.util.ArrayList;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springbootfirst.entity.Employee;
@@ -10,18 +13,36 @@ import com.springbootfirst.entity.Employee;
 @RestController
 public class FbController {
 	
-	@RequestMapping("hello")
+	@GetMapping("hello")
 	public String hello() {
 		return "Hello there! Welcome to Spring Boot";
 	}
 	
-	@RequestMapping("empList")
+	@GetMapping("empList")
 	public ArrayList<Employee> empList(){
 		ArrayList<Employee> empList = new ArrayList<Employee>();
 		empList.add(new Employee("Bhavik", "bhavik@gmail.com", "pass@bhavik"));
 		empList.add(new Employee("Naman", "naman@gmail.com", "pass@naman"));
+		System.out.println(empList);
 		return empList;
 	}
+	
+	@PostMapping("hello")
+	public String post() {
+		return "@PostMapping there! Welcome to Spring Boot";
+	}
+	
+	@PutMapping("hello")
+	public String put() {
+		return "@PutMapping there! Welcome to Spring Boot";
+	}
+	
+	@DeleteMapping("hello")
+	public String delete() {
+		return "@DeleteMapping there! Welcome to Spring Boot";
+	}
+	
+	
 	
 
 }
