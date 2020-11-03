cfa717cb8cb33a509c7644a1fed096d6cf604141
Bhavik Patel <bhavikpatel143513@gmail.com>
2020-11-03 10:20:05 +0530

    @EnableEureka Server annotation in both consumer and producer

--- springbootfirst/src/main/java/com/springbootfirst/SpringbootfirstApplication.java
+++ springbootfirst/src/main/java/com/springbootfirst/SpringbootfirstApplication.java
@@ -2,8 +2,10 @@ package com.springbootfirst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootfirstApplication {
 
 	public static void main(String[] args) {
